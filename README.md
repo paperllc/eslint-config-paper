3f95112089bd0e9b2a7ac86e9f94fa60f5de873d
amatiash <a.matiashevich@gmail.com>
2019-10-16 14:44:37 +0300

    Added ESLint rules

---
+++ README.md
@@ -0,0 +1,21 @@
+# eslint-config-paper
+
+## Install
+
+First install this package:
+
+`npm i git://github.com/amatiash/eslint-config-paper.git#semver:^0.1.0 -D`
+
+Then peerDependencies:
+
+`npm i eslint@^6.5.0 typescript@^3.6.0 eslint-plugin-react@^7.16.0 @typescript-eslint/parser@^2.4.0 @typescript-eslint/eslint-plugin@^2.4.0 -D`
+
+## Usage
+
+In `.eslintrc.json`
+
+```json
+{
+    "extends": "paper"
+}
+```
---
+++ index.js
@@ -0,0 +1,216 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: [
+        '@typescript-eslint',
+        'react',
+    ],
+    rules: {
+
+        // Possible Errors
+        // ----------------------------------------------------
+
+        'getter-return': 'warn',
+        'no-async-promise-executor': 'warn',
+        'no-compare-neg-zero': 'warn',
+        'no-console': [
+            'warn',
+            {
+                allow: [
+                    'warn',
+                    'error',
+                ],
+            },
+        ],
+        'no-control-regex': 'error',
+        'no-empty': 'warn',
+        'no-empty-character-class': 'error',
+        'no-extra-boolean-cast': 'warn',
+        'no-func-assign': 'error',
+        'no-import-assign': 'error',
+        'no-inner-declarations': 'error',
+        'no-irregular-whitespace': 'error',
+        'no-misleading-character-class': 'error',
+        'no-prototype-builtins': 'warn',
+        'no-regex-spaces': 'warn',
+        'no-unsafe-negation': 'warn',
+        'require-atomic-updates': 'warn',
+
+        // Best Practices
+        // ----------------------------------------------------
+
+        'array-callback-return': [
+            'warn',
+            {
+                allowImplicit: true,
+            },
+        ],
+        'consistent-return': 'warn',
+        'default-param-last': 'warn',
+        'max-classes-per-file': [
+            'error',
+            1,
+        ],
+        'no-alert': 'error',
+        'no-case-declarations': 'error',
+        'no-else-return': [
+            'warn',
+            {
+                allowElseIf: false,
+            },
+        ],
+        'no-empty-function': [
+            'warn',
+            {
+                allow: [
+                    'arrowFunctions',
+                    'functions',
+                    'methods',
+                ],
+            },
+        ],
+        'no-empty-pattern': 'warn',
+        'no-eval': 'error',
+        'no-extend-native': 'error',
+        'no-extra-bind': 'warn',
+        'no-extra-label': 'warn',
+        'no-floating-decimal': 'warn',
+        'no-global-assign': 'error',
+        'no-implied-eval': 'error',
+        'no-iterator': 'error',
+        'no-loop-func': 'warn',
+        'no-new-func': 'error',
+        'no-octal': 'error',
+        'no-octal-escape': 'error',
+        'no-proto': 'error',
+        'no-return-await': 'warn',
+        'no-script-url': 'error',
+        'no-self-compare': 'warn',
+        'no-throw-literal': 'error',
+        'no-useless-escape': 'warn',
+        'no-with': 'error',
+        'prefer-promise-reject-errors': [
+            'error',
+            {
+                allowEmptyReject: true,
+            },
+        ],
+        'wrap-iife': [
+            'warn',
+            'inside',
+            {
+                functionPrototypeMethods: true,
+            },
+        ],
+        yoda: 'warn',
+
+        // Strict Mode
+        // ----------------------------------------------------
+
+        strict: ['warn', 'never'],
+
+        // Variables
+        // ----------------------------------------------------
+
+        'no-label-var': 'error',
+        'no-undef-init': 'warn',
+        'no-use-before-define': [
+            'error',
+            {
+                functions: false,
+                classes: true,
+                variables: true,
+            },
+        ],
+
+        // Node.js and CommonJS
+        // ----------------------------------------------------
+
+        'no-path-concat': 'error',
+
+        // Stylistic Issues
+        // ----------------------------------------------------
+
+        camelcase: [
+            'error',
+            {
+                properties: 'never',
+                ignoreDestructuring: false,
+            },
+        ],
+        'eol-last': 'error',
+        'jsx-quotes': ['warn', 'prefer-double'],
+        quotes: ['warn', 'single', {avoidEscape: true}],
+        'quote-props': ['warn', 'as-needed'],
+        'func-style': [
+            'error',
+            'declaration',
+            {
+                allowArrowFunctions: true,
+            },
+        ],
+        'linebreak-style': [
+            'error',
+            'unix',
+        ],
+        'new-cap': [
+            'warn',
+            {
+                newIsCap: true,
+                capIsNew: false,
+            },
+        ],
+        'no-array-constructor': 'error',
+        'no-bitwise': [
+            'error',
+            {
+                allow: [
+                    '~',
+                ],
+            },
+        ],
+        'no-lonely-if': 'warn',
+        'no-multi-assign': 'error',
+        'nonblock-statement-body-position': [
+            'warn',
+            'below',
+        ],
+        'one-var': [
+            'warn',
+            'consecutive',
+        ],
+        'unicode-bom': 'error',
+
+        // ECMAScript 6
+        // ----------------------------------------------------
+
+        'arrow-parens': 'warn',
+        'object-shorthand': [
+            'warn',
+            'always',
+            {
+                avoidQuotes: true,
+            },
+        ],
+        'prefer-const': [
+            'warn',
+            {
+                destructuring: 'any',
+                ignoreReadBeforeAssign: true,
+            },
+        ],
+        'prefer-numeric-literals': 'error',
+        'prefer-rest-params': 'warn',
+        'symbol-description': 'error',
+    },
+}
